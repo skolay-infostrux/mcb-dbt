bf4867b4e0b49d22bd5591896ab98ff8929ca668
skolay-infostrux <103535202+skolay-infostrux@users.noreply.github.com>
2024-02-27 19:22:48 -0500

    Add files via upload

---
+++ models/clean/b4b/compliant/b4b__stg_compliant_customer.sql
@@ -0,0 +1,23 @@
+{{ config(
+    tags=["clean","b4b","compliant"],
+    schema='b4b',
+    pre_hook="
+
+        copy into {{ var('project_database_prefix') }}_{{ target.name }}_INGEST.{{ env_var('DBT_SNOWFLAKE_SCHEMA_PREFIX', '') }}B4B.RAW_CUSTOMER(DATA_REC,FILE_ROW_NUMBER,FILE_NAME,FILE_ROW_KEY,FILE_INSERT_TS,MAX_ROW_NUMBER) 
+            from (
+                select 
+                    $1  as DATA_REC
+                    ,metadata$file_row_number as FILE_ROW_NUMBER
+                    ,metadata$filename as FILE_NAME
+                    ,CAST( MD5( NVL(CAST(DATA_REC as varchar),'null') ) as varchar ) as FILE_ROW_KEY
+                    ,current_timestamp as FILE_INSERT_TS
+                    ,max(metadata$file_row_number) OVER (PARTITION BY metadata$filename ) as MAX_ROW_NUMBER
+                from '@{{ var('project_database_prefix') }}_{{ target.name }}_INGEST.{{ env_var('DBT_SNOWFLAKE_SCHEMA_PREFIX', '') }}B4B.EXT_STG_PREPAID/COMPLIANT/'
+            ) 
+        FILE_FORMAT = (FORMAT_NAME={{ var('project_database_prefix') }}_{{ target.name }}_INGEST.{{ env_var('DBT_SNOWFLAKE_SCHEMA_PREFIX', '') }}B4B.FF_COMPLIANT_B4B)
+        PATTERN = '.*/B4_CUST_.*.TXT.*';
+        "  
+    )
+}}
+
+{{ generate_stg_compliant_customer('raw_data_b4b','raw_customer','y') }}
